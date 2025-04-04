7a4a00506e8aa9a852a422bcb74a188936b065e6
LeHuyVuu <lehuyvuok@gmail.com>
2025-04-04 15:04:46 +0700

    from vu

--- src/app/context/store.js
+++ src/app/context/store.js
@@ -1,3 +1,3 @@
 import { signify } from 'react-signify';
 
-export const sCountItem = signify(JSON.parse(localStorage.getItem("cartItems"))?.length);
+export const sCountItem = signify(JSON.parse(localStorage.getItem("cartItems"))?.length || 0);
